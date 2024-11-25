b8b2f16c634ad0e3f003f21c15f1b6395b329777
kelly bian <81708806+kellydoesstuff@users.noreply.github.com>
2024-11-24 20:58:30 -0600

    Update util.hpp

--- include/gheese/util.hpp
+++ include/gheese/util.hpp
@@ -1,3 +1,9 @@
 namespace gheese {
     int sgn(double _input);
+    enum exit_output { RUNNING = 1,
+                       SMALL_EXIT = 2,
+                       BIG_EXIT = 3,
+                       VELOCITY_EXIT = 4,
+                       mA_EXIT = 5,
+                       ERROR_NO_CONSTANTS = 6 };
 }
